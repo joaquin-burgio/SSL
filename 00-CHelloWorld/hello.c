d7b2eff860d6522285e9175fee85faf0763b49da
utn_joaquin <joaquinburgio@gmail.com>
2024-04-15 21:24:35 -0300

    Creacion de archivo txt

--- 00-CHelloWorld/hello.c
+++ 00-CHelloWorld/hello.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hola mundo");
+    FILE *fp;
+    fp = fopen("output.txt", "w"); // Abrir el archivo en modo escritura ("w")
+
+    if (fp == NULL) {
+        printf("Error al abrir el archivo.\n");
+        return 1;
+    }
+
+    fprintf(fp, "Hello, World!\n"); // Escribir en el archivo
+
+    fclose(fp); // Cerrar el archivo
     return 0;
 }

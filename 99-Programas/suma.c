0172c71b1b22050aeffc0c137713e8b0d4c75973
utn_joaquin <joaquinburgio@gmail.com>
2024-06-25 15:09:47 -0300

    vuelvo atras

---
+++ 99-Programas/suma.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int esNumero(const char *nro) {
+    int ret=1;
+    int i=0;
+    char c = nro[0];
+    while( c!='\0' && ret!=0 )
+    {
+        if(isdigit(c)==0)//el caracter no es un digito
+        {
+            ret = 0;
+        }
+        i++;
+        c = nro[i];
+    }
+	return ret;
+}
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        puts("uso: suma nro1 nro2");
+        exit(0);
+    }
+    if (!esNumero(argv[1]) || !esNumero(argv[2])) {
+        puts("nro1 y nro2 deben ser constantes numéricas");
+        exit(-1);
+    }
+    int sum1 = atoi(argv[1]);
+    int sum2 = atoi(argv[2]);
+    printf("la suma de %d y %d es %d", sum1, sum2, sum1+sum2);
+    return 0;
+}
